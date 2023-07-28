5876b05fba0ee7d6a0dbc9d14ba9cd026593eb75
Vladislav Volkov <volcharneverfall1996@gmail.com>
2023-07-28 12:19:58 +0300

    Fix docker-compose call after certificate update
    Use `/usr/local/bin/docker-compose` to perform Nginx restart to avoid crontab error:
    
    ```
    /home/ec2-user/redash-setup/data/ssl_certificate_renew.sh: line 10: docker-compose: command not found
    ```

--- data/ssl_certificate_renew.sh
+++ data/ssl_certificate_renew.sh
@@ -7,4 +7,5 @@ docker run -t --rm \
 
 # Ensure that docker-compose.yml is accessible via `/home/ec2-user/redash-setup/data/docker-compose.yml` path
 # or change `/home/ec2-user/redash-setup/data/docker-compose.yml` to path to Redash compose file.
-docker-compose -f /home/ec2-user/redash-setup/data/docker-compose.yml exec nginx nginx -s reload
+# Also please ensure that `/usr/local/bin/docker-compose` is path to docker-compose binary.
+/usr/local/bin/docker-compose -f /home/ec2-user/redash-setup/data/docker-compose.yml exec nginx nginx -s reload
